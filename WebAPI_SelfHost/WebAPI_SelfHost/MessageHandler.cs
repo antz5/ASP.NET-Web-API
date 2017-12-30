6e36da49bf02a00742fa72496e0992d7e434bc69
ananth <ananthsmurthy@hotmail.com>
2017-12-30 13:44:22 +1100

    Simple SelfHosting using Console application

---
+++ WebAPI_SelfHost/WebAPI_SelfHost/MessageHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebAPI_SelfHost
+{
+    class MessageHandler : HttpMessageHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            Console.WriteLine("Recieved an http message");
+            var task = new Task<HttpResponseMessage>(()=>
+            {
+                var msg = new HttpResponseMessage();
+                msg.Content = new StringContent("Self-Hosting");
+                Console.WriteLine("Http response returned");
+                return msg;
+            });
+            task.Start();
+            return task;
+        }        
+    }
+}
---
+++ WebAPI_SelfHost/WebAPI_SelfHost/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web.Http.SelfHost;
+
+namespace WebAPI_SelfHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new HttpSelfHostConfiguration("http://localhost:8999");
+
+            var server = new HttpSelfHostServer(config, new MessageHandler());
+            var task = server.OpenAsync();
+            task.Wait();
+            Console.WriteLine("Server is up and running");
+            Console.ReadLine();
+        }
+    }
+}
